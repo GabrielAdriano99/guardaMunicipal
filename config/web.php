a73f083b6476ccdbb1b670362db900e42c1588dc
GabrielAdriano99 <gabrielpapel8@gmail.com>
2019-05-30 08:55:25 -0300

    Criação de layout

--- config/web.php
+++ config/web.php
@@ -12,6 +12,7 @@ $config = [
         '@npm'   => '@vendor/npm-asset',
     ],
     'language' => 'pt-BR',
+    'layout' => '_main',
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
